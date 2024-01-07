08ac355c8fd8873a3a3b32dc278872ba7e63c8a0
kaluczadzsi <kaluczadzsi@gmail.com>
2024-01-07 11:29:32 +0100

    setting classes and text conditionally

--- 03-pizza-menu/starter/src/components/Pizza.jsx
+++ 03-pizza-menu/starter/src/components/Pizza.jsx
@@ -1,12 +1,12 @@
 /* eslint-disable react/prop-types */
-const Pizza = ({ name, ingredients, photoName, price }) => {
+const Pizza = ({ name, ingredients, photoName, price, soldOut }) => {
   return (
-    <div className="pizza">
+    <div className={`pizza ${soldOut ? 'sold-out' : ''}`}>
       <img src={photoName} alt={name} />
       <div>
         <h3>{name}</h3>
         <p>{ingredients}</p>
-        <span>{price}</span>
+        <span>{soldOut ? `Sold out` : price}</span>
       </div>
     </div>
   );
