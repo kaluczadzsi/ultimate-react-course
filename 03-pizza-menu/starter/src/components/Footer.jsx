2f1f2912cfac19070c79ffa92a2a7d12cd4df43b
kaluczadzsi <kaluczadzsi@gmail.com>
2024-01-07 11:16:20 +0100

    conditional rendering with ternaries

--- 03-pizza-menu/starter/src/components/Footer.jsx
+++ 03-pizza-menu/starter/src/components/Footer.jsx
@@ -8,13 +8,17 @@ const Footer = () => {
   return (
     <>
       <footer className="footer">
-        {isOpen && (
+        {isOpen ? (
           <div className="order">
             <p>
               We're open until {closeHour}:00. Come visit us or order online.
             </p>
             <button className="btn">Order</button>
           </div>
+        ) : (
+          <p>
+            We're happy to welcome you between {openHour}:00 and {closeHour}:00.
+          </p>
         )}
       </footer>
     </>
--- 03-pizza-menu/starter/src/components/Menu.jsx
+++ 03-pizza-menu/starter/src/components/Menu.jsx
@@ -9,12 +9,14 @@ const Menu = () => {
     <main className="menu">
       <h2>Our menu</h2>
 
-      {numPizzas > 0 && (
+      {numPizzas > 0 ? (
         <ul className="pizzas">
           {pizzas.map((pizza) => (
             <Pizza key={pizza.name} {...pizza} />
           ))}
         </ul>
+      ) : (
+        <p>We're still working on our menu. Please come back later :)</p>
       )}
     </main>
   );
