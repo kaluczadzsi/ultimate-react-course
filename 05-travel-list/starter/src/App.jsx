9434babd2cfcbeb41b000c03baa5796d16c047f9
kaluczadzsi <kaluczadzsi@gmail.com>
2024-01-08 22:19:50 +0100

    building the layout

--- 05-travel-list/starter/src/App.jsx
+++ 05-travel-list/starter/src/App.jsx
@@ -1,5 +1,16 @@
+import Logo from './components/Logo';
+import Form from './components/Form';
+import Stats from './components/Stats';
+import PackingList from './components/PackingList';
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div className="app">
+      <Logo />
+      <Form />
+      <PackingList />
+      <Stats />
+    </div>
+  );
 };
 
 export default App;
---
+++ 05-travel-list/starter/src/components/Stats.jsx
@@ -0,0 +1,9 @@
+const Stats = () => {
+  return (
+    <footer className="stats">
+      You have X items on list, and you already packed X (%)
+    </footer>
+  );
+};
+
+export default Stats;
