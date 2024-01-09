3f97dfdd7c540af35ab154a246c4af58c6ba2ceb
kaluczadzsi <kaluczadzsi@gmail.com>
2024-01-09 21:19:20 +0100

    building a form & handling submissions

--- 05-travel-list/starter/src/components/Form.jsx
+++ 05-travel-list/starter/src/components/Form.jsx
@@ -1,7 +1,20 @@
 const Form = () => {
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
   return (
-    <div className="add-form">
+    <div className="add-form" onSubmit={handleSubmit}>
       <h3>What do you need for your 😍 trip?</h3>
+      <select>
+        {Array.from({ length: 20 }, (_, i) => i + 1).map((num) => (
+          <option value={num} key={num}>
+            {num}
+          </option>
+        ))}
+      </select>
+      <input type="text" placeholder="Item..." />
+      <button>Add</button>
     </div>
   );
 };
