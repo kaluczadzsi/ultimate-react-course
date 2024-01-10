3fb52e424e14a2804db69b6293a185ecfbaccd8a
kaluczadzsi <kaluczadzsi@gmail.com>
2024-01-10 19:12:27 +0100

    controlled elements

--- 05-travel-list/starter/src/components/Form.jsx
+++ 05-travel-list/starter/src/components/Form.jsx
@@ -1,21 +1,45 @@
+import { useState } from 'react';
+
 const Form = () => {
+  const [description, setDescription] = useState('');
+  const [quantity, setQuantity] = useState('');
+
   const handleSubmit = (e) => {
     e.preventDefault();
+
+    if (!description) return;
+
+    const newItem = {
+      id: crypto.randomUUID().slice(0, 5),
+      description,
+      quantity,
+      packed: false,
+    };
+
+    setDescription('');
+    setQuantity(1);
   };
 
   return (
-    <div className="add-form" onSubmit={handleSubmit}>
+    <form className="add-form" onSubmit={handleSubmit}>
       <h3>What do you need for your 😍 trip?</h3>
-      <select>
+      <select value={quantity} onChange={(e) => setQuantity(e.target.value)}>
         {Array.from({ length: 20 }, (_, i) => i + 1).map((num) => (
           <option value={num} key={num}>
             {num}
           </option>
         ))}
       </select>
-      <input type="text" placeholder="Item..." />
+      <input
+        type="text"
+        placeholder="Item..."
+        value={description}
+        onChange={(e) => {
+          setDescription(e.target.value);
+        }}
+      />
       <button>Add</button>
-    </div>
+    </form>
   );
 };
 
