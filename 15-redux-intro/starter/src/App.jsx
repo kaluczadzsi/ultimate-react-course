19ef3dc678e3daeba024bfd1f5e0cf020dec3446
kaluczadzsi <kaluczadzsi@gmail.com>
2024-02-04 16:41:11 +0100

    creating a redux store

--- 15-redux-intro/starter/src/App.jsx
+++ 15-redux-intro/starter/src/App.jsx
@@ -1,7 +1,18 @@
-import React from 'react';
+import CreateCustomer from "./CreateCustomer";
+import Customer from "./Customer";
+import AccountOperations from "./AccountOperations";
+import BalanceDisplay from "./BalanceDisplay";
 
-const App = () => {
-  return <div>App</div>;
-};
+function App() {
+  return (
+    <div>
+      <h1>🏦 The React-Redux Bank ⚛️</h1>
+      <CreateCustomer />
+      <Customer />
+      <AccountOperations />
+      <BalanceDisplay />
+    </div>
+  );
+}
 
 export default App;
