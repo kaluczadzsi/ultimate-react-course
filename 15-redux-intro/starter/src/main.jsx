b5be96543199a23c0b4e5b690c0911cb364be8f7
kaluczadzsi <kaluczadzsi@gmail.com>
2024-02-04 17:21:00 +0100

    connecting our redux app with react

--- 15-redux-intro/starter/src/main.jsx
+++ 15-redux-intro/starter/src/main.jsx
@@ -1,10 +1,13 @@
+import './index.css';
+
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
-import './index.css';
+import { Provider } from 'react-redux';
 
 import store from './store.js';
 
-store.dispatch({ type: 'account/deposit', payload: 250 });
-console.log(store.getState());
-
-ReactDOM.createRoot(document.getElementById('root')).render(<App />);
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <Provider store={store}>
+    <App />
+  </Provider>
+);
